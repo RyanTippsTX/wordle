f451b5fe7b44e3c95a6bbd2433a9083b1955f1ad
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2025-02-21 02:23:04 -0600

    spin animation on share

--- app/components/end.tsx
+++ app/components/end.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { WordleIcon } from './wordle-icon';
+import { twMerge } from 'tailwind-merge';
 
 const shareMessage = `Wordle 1,342 3/6
 
@@ -7,9 +9,23 @@ const shareMessage = `Wordle 1,342 3/6
 🟩🟩🟩🟩🟩`;
 
 export function End() {
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  const handleSpin = () => {
+    setIsSpinning(true);
+    setTimeout(() => {
+      setIsSpinning(false);
+    }, 400);
+  };
+
   return (
     <div className="absolute inset-0 text-gray-100 flex flex-col items-center justify-center bg-neutral-900">
-      <div className="mb-3 w-16 h-16 flex items-center justify-center">
+      <div
+        className={twMerge(
+          'mb-3 w-16 h-16 flex items-center justify-center',
+          isSpinning && 'animate-rapid-spin',
+        )}
+      >
         <WordleIcon />
       </div>
 
@@ -19,8 +35,10 @@ export function End() {
       <button
         className="w-40 h-12 py-2 px-4 bg-gray-100 text-black rounded-full border border-black"
         onClick={() => {
+          handleSpin();
           navigator.clipboard.writeText(shareMessage);
         }}
+        disabled={isSpinning}
       >
         Share
       </button>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      keyframes: {
+        'rapid-spin': {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' }, // 3 full spins
+        },
+      },
+      animation: {
+        'rapid-spin': 'rapid-spin 400ms ease-out',
+      },
+    },
+  },
 };
