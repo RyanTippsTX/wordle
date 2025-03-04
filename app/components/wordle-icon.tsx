fc3f3b32dbf0bd6f9d90c15d24226cc3b119cc8e
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2025-03-04 00:50:24 -0600

    fix responsive logo sizing

--- app/components/wordle-icon.tsx
+++ app/components/wordle-icon.tsx
@@ -1,8 +1,9 @@
 export function WordleIcon() {
   return (
     <svg
-      // width="62"
-      // height="62"
+      className="flex-1"
+      width="62"
+      height="62"
       viewBox="0 0 62 62"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
