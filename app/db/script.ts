1706772d897869d6cb33e3c34ec33917c2d037ed
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2025-03-05 01:27:14 -0600

    serve game data from DB

--- app/db/script.ts
+++ app/db/script.ts
@@ -1,9 +1,6 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/neon-http';
 import { eq } from 'drizzle-orm';
-import { gamesTable } from './schema';
-
-const db = drizzle(process.env.DATABASE_URL!);
+import { db, gamesTable } from './schema';
 
 async function main() {
   const game: typeof gamesTable.$inferInsert = {
--- app/utils/game.ts
+++ app/utils/game.ts
@@ -1,38 +1,35 @@
 import { createServerFn } from '@tanstack/start';
+import { db, gamesTable } from '../db/schema';
+import { eq } from 'drizzle-orm';
 
-export type Game = {
-  id: number; // e.g.  Tippsle #108
-  date: string; // YYYY-MM-DD
-  solution: string;
-  chosenBy: string | null;
+const fallBackGame: typeof gamesTable.$inferSelect = {
+  id: 0,
+  date: '1999-12-31',
+  solution: 'house',
+  chosenBy: 'Doris T.',
 };
 
 export const getTodaysGame = createServerFn({ method: 'GET' })
   // .validator((id: string) => id)
   .handler(async ({ data }) => {
-    // TODO: get from database
-    const id = 11;
-    // const date = new Intl.DateTimeFormat('en-CA', {
-    //   timeZone: 'America/Chicago', // Central Time (CST/CDT)
-    //   year: 'numeric',
-    //   month: '2-digit',
-    //   day: '2-digit',
-    // }).format(new Date());
-    const date = '2024-11-17';
-    const solution = 'house';
-    const chosenBy = 'Doris T.';
+    /** YYYY-MM-DD in server's timezone */
+    const today = new Intl.DateTimeFormat('en-CA', {
+      timeZone: 'America/Chicago', // Central Time (CST/CDT)
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    }).format(new Date());
 
-    // fake await 100ms
-    await new Promise((resolve) => setTimeout(resolve, 100));
+    const games = await db
+      .select()
+      .from(gamesTable)
+      // .where(eq(gamesTable.id, 11))
+      .where(eq(gamesTable.date, today));
 
-    const todaysGame: Game = {
-      id,
-      date,
-      solution,
-      chosenBy,
-    };
+    const todaysGame = games[0];
+    console.log('🔥 todaysGame', todaysGame);
 
-    return todaysGame;
+    return todaysGame || fallBackGame;
   });
 
 export const saveGameState = createServerFn({ method: 'POST' })
