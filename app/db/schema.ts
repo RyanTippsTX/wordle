091d5bef5ee316a25a472a41a528703510067f22
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2025-03-05 00:28:21 -0600

    schema for games, plays, and word list

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,8 +1,40 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import {
+  date,
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  boolean,
+  serial,
+  varchar,
+} from 'drizzle-orm/pg-core';
 
-export const usersTable = pgTable('users', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+// represents each day's game definition
+export const gamesTable = pgTable('games', {
+  id: serial().primaryKey(),
+  date: date().unique().notNull().defaultNow(),
+  solution: varchar({ length: 5 }).notNull(),
+  chosenBy: text(),
+});
+
+// represents each play of the game
+export const playsTable = pgTable('plays', {
+  id: uuid().primaryKey().defaultRandom(),
+  gameId: integer()
+    .notNull()
+    .references(() => gamesTable.id), // which day/game
+  guessCount: integer().notNull(), // not counted until they make initial guess
+  solved: boolean().notNull().default(false),
+  // startedAt: timestamp().notNull().defaultNow(), // consider adding in future
+  firstGuessAt: timestamp().notNull().defaultNow(),
+  lastGuessAt: timestamp().notNull().defaultNow(),
+});
+
+// word list, for guesses & answers
+export const wordsTable = pgTable('words', {
+  id: uuid().primaryKey().defaultRandom(),
+  word: varchar({ length: 5 }).unique().notNull(),
+  // isEligibleSolution: boolean().notNull().default(true), // consider adding in future
+  createdAt: timestamp().notNull().defaultNow(), // unused, but why not
 });
