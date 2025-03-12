387925215ff0f83f6d1025f2de2164298dfbaea4
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2025-03-11 21:05:53 -0500

    override dev budling related error on localhost

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,9 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 
+// hacky fix, if error persists then wrap this in a db init function
+const isServer = () => typeof window === 'undefined';
+if (!isServer()) throw new Error('Ignore this error if you are on the client on localhost.');
+
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export const db = drizzle({
